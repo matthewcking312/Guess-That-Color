bc8b538ae26bcbf9e2c51ec635b1f234d1350c83
matthewcking312 <matthewcking312@gmail.com>
2018-11-11 18:58:46 -0800

    Changes made to include the basic functionality of the site. Further editing to still occur.

--- script.js
+++ script.js
@@ -8,7 +8,24 @@ var colors = [
 ]
 
 var squares = document.querySelectorAll(".square");
+var pickedColor = colors[3];
+var colorDisplay = document.getElementById("colorDisplay");
+
+colorDisplay.textContent = pickedColor;
 
 for(var i = 0; i < squares.length; i++){
+	// add the initial colors to squares
 	squares[i].style.backgroundColor = colors[i];
+
+	// add click listeners to squares
+	squares[i].addEventListener("click", function(){
+		// First we need to grab the color of the clicked square
+		var clickedColor = this.style.backgroundColor;
+		// Then we are going to compare it to the picked color
+		if (clickedColor === pickedColor) {
+			alert("Correct!");
+		} else {
+			alert("WRONG!");
+		}
+	});
 }
