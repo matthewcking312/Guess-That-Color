c2b2d43bd5e9b6ebf5a1255097c60dd981781ea6
matthewcking312 <matthewcking312@gmail.com>
2018-11-12 19:55:22 -0800

    Fully functional game. Still needs proper styling and refactoring.

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-var colors = generateRandomColors(6);
+var numSquares = 6;
+var colors = generateRandomColors(numSquares);
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
@@ -11,7 +12,8 @@ var hardBtn = document.querySelector("#hardBtn");
 easyBtn.addEventListener("click", function(){
 	hardBtn.classList.remove("selected");
 	easyBtn.classList.add("selected");
-	colors = generateRandomColors(3);
+	numSquares = 3;
+	colors = generateRandomColors(numSquares);
 	pickedColor = pickColor();
 	colorDisplay.textContent = pickedColor;
 	for(var i = 0; i < squares.length; i++){
@@ -26,12 +28,20 @@ easyBtn.addEventListener("click", function(){
 hardBtn.addEventListener("click", function(){
 	hardBtn.classList.add("selected");
 	easyBtn.classList.remove("selected");
+	numSquares = 6;
+	colors = generateRandomColors(numSquares);
+	pickedColor = pickColor();
+	colorDisplay.textContent = pickedColor;
+	for(var i = 0; i < squares.length; i++){
+		squares[i].style.backgroundColor = colors[i];
+		squares[i].style.display = "none";
+	}
 });
 
 resetButton.addEventListener("click", function(){
 	// When the button is clicked we need to be ale to generate 
 	// all new colors
-	colors = generateRandomColors(6);
+	colors = generateRandomColors(numSquares);
 	// pick a new random color from an array
 	pickedColor = pickColor();
 	// change colorDisplay to match picked color
