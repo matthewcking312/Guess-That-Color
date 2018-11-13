58946d3e8d6f1216f2245b1321a5db1a9c8b3db0
matthewcking312 <matthewcking312@gmail.com>
2018-11-12 19:30:12 -0800

    Addition of easy and hard mode included. Now on easy mode, only 3 squares will be chosen from.

--- script.js
+++ script.js
@@ -1,9 +1,47 @@
 var colors = generateRandomColors(6);
-
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
+var h1 = document.querySelector("h1");
+var resetButton = document.querySelector("#reset");
+var easyBtn = document.querySelector("#easyBtn");
+var hardBtn = document.querySelector("#hardBtn");
+
+easyBtn.addEventListener("click", function(){
+	hardBtn.classList.remove("selected");
+	easyBtn.classList.add("selected");
+	colors = generateRandomColors(3);
+	pickedColor = pickColor();
+	colorDisplay.textContent = pickedColor;
+	for(var i = 0; i < squares.length; i++){
+		if (colors[i]) {
+			squares[i].style.backgroundColor = colors[i];
+		} else {
+			squares[i].style.display = "none";
+		}
+	}
+});
+
+hardBtn.addEventListener("click", function(){
+	hardBtn.classList.add("selected");
+	easyBtn.classList.remove("selected");
+});
+
+resetButton.addEventListener("click", function(){
+	// When the button is clicked we need to be ale to generate 
+	// all new colors
+	colors = generateRandomColors(6);
+	// pick a new random color from an array
+	pickedColor = pickColor();
+	// change colorDisplay to match picked color
+	colorDisplay.textContent = pickedColor;
+	// change colors of the squares
+	for(var i = 0; i < squares.length; i++){
+		squares[i].style.backgroundColor = colors[i];
+	}
+	h1.style.backgroundColor = "#232323";
+});
 
 colorDisplay.textContent = pickedColor;
 
@@ -16,9 +54,13 @@ for(var i = 0; i < squares.length; i++){
 		// First we need to grab the color of the clicked square
 		var clickedColor = this.style.backgroundColor;
 		// Then we are going to compare it to the picked color
+		console.log(clickedColor, pickedColor);
 		if (clickedColor === pickedColor) {
 			messageDisplay.textContent = "Correct!";
+			resetButton.textContent = "Play Again?";
 			changeColors(clickedColor);
+			h1.style.backgroundColor = clickedColor;
+
 		} else {
 			this.style.backgroundColor = "#232323";
 			messageDisplay.textContent = "Try Again";
